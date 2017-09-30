082893e404e8fdfa1b30c90e8d6290288cf6a2e2
Alessandro Buggin <alessandrobuggin@gmail.com>
2017-09-30 20:45:59 +0200

    * master: green. time to move

--- src/test/scala/gol/Core.scala
+++ src/test/scala/gol/Core.scala
@@ -3,10 +3,16 @@ package gol
 import org.scalatest.{FlatSpec, Matchers}
 
 class Core extends FlatSpec with Matchers {
-  "An empty Universe" should {
-    "tick" in {
-      val emptyUniverse: Universe = Universe.empty
-      emptyUniverse.tick shouldBe an[Universe]
-    }
+  "An empty Universe" should "tick" in {
+    val emptyUniverse: Universe = Universe.empty
+    emptyUniverse.tick shouldBe an[Universe]
   }
+}
+
+case class Universe() {
+  def tick: Universe = this
+}
+
+case object Universe {
+  def empty: Universe = Universe()
 }
